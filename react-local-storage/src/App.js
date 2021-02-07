cb5f36d463d125b72515999c55d80ad89b7604ac
Valerie Ren <zhaorui.ren@pm.me>
2021-02-07 16:35:20 +0100

    example of local storage and session storage

--- react-local-storage/src/App.js
+++ react-local-storage/src/App.js
@@ -1,23 +1,57 @@
+import React, {useState, useEffect} from 'react';
 import './App.css';
 
 function App() {
+  const [value, setValue] = useStateWithLocalStorage(
+    'localStorage'
+  );
+  const [session, setSession] = useStateWithSessionStorage(
+    'sessionStorage'
+  );
+ 
+  const onChangeLocalStorage = event => setValue(event.target.value);
+ 
+  const onChangeSessionStorage = event => setSession(event.target.value);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>React with Local Storage!</h1>
+ 
+      <input value={value} type="text" onChange={onChangeLocalStorage} />
+ 
+      <p>{value}</p>
+
+      <h1>React with Session Storage!</h1>
+ 
+      <input value={session} type="text" onChange={onChangeSessionStorage} />
+
+      <p>{session}</p>
     </div>
   );
 }
+
+const useStateWithSessionStorage = sessionStorageKey => {
+  const [value, setValue] = useState(
+    sessionStorage.getItem(sessionStorageKey) || ''
+  );
+ 
+  useEffect(() => {
+    sessionStorage.setItem(sessionStorageKey, value);
+  }, [value]);
+ 
+  return [value, setValue];
+};
+
+const useStateWithLocalStorage = localStorageKey => {
+  const [value, setValue] = useState(
+    localStorage.getItem(localStorageKey) || ''
+  );
+ 
+  useEffect(() => {
+    localStorage.setItem(localStorageKey, value);
+  }, [value]);
+ 
+  return [value, setValue];
+};
 
 export default App;
